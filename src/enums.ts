bdbc7385c056776c4a37e6852b1aa1884766d54b
Edward Arechwa <e.arechwa@gmail.com>
2022-04-09 13:28:16 +0200

    enums

---
+++ src/enums.ts
@@ -0,0 +1,19 @@
+enum RoleEnum1 {
+  ADMIN,
+  USER,
+}
+
+enum RoleEnum2 {
+  ADMIN = 1,
+  USER,
+}
+
+enum RoleEnum3 {
+  ADMIN = 1,
+  USER = "string",
+}
+
+enum RoleEnum4 {
+  ADMIN = "admin",
+  USER = "string",
+}
