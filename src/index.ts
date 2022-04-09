f91ab550f68da6b20fe32b61d1dbd34a9ca4c933
Edward Arechwa <e.arechwa@gmail.com>
2022-04-09 12:14:28 +0200

    variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const number: number = 1;
+
+const string: string = "1";
+
+const boolean: boolean = true;
+
+const arr: number[] = [1, 2, 3];
+
+const arr1: Array<number> = [1, 2, 3];
